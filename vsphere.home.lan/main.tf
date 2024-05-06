8aabeae2072c3e5085d8998369589b7ff460bdd2
louis campbell <louis@campbell.lu>
2024-05-06 11:35:36 +0100

    Remove folders, modify VM names, and add tags in main.tf
    The commit deletes the unnecessary resource folders and renames the virtual machines in the main.tf file to a new naming convention. It also introduced a new tagging system to help better identify VMs. The modifications aim to enhance organization in the environment.

--- vsphere.home.lan/main.tf
+++ vsphere.home.lan/main.tf
@@ -69,30 +69,6 @@ resource "vsphere_host" "esxi_host_02" {
   thumbprint = data.vsphere_host_thumbprint.esxi_host_02_thumbprint.id
 }
 
-resource "vsphere_folder" "linux_servers_folder" {
-  path          = "Linux Servers"
-  type          = "vm"
-  datacenter_id = vsphere_datacenter.homelab_datacenter.moid
-}
-
-resource "vsphere_folder" "windows_servers_folder" {
-  path          = "Windows Servers"
-  type          = "vm"
-  datacenter_id = vsphere_datacenter.homelab_datacenter.moid
-}
-
-resource "vsphere_folder" "systems_folder" {
-  path          = "Core Systems"
-  type          = "vm"
-  datacenter_id = vsphere_datacenter.homelab_datacenter.moid
-}
-
-resource "vsphere_folder" "workstations_folder" {
-  path          = "Workstations"
-  type          = "vm"
-  datacenter_id = vsphere_datacenter.homelab_datacenter.moid
-}
-
 resource "vsphere_virtual_machine" "truenas_core" {
   lifecycle {
     ignore_changes = [
@@ -100,8 +76,8 @@ resource "vsphere_virtual_machine" "truenas_core" {
       hv_mode
     ]
   }
-  folder = "/Core Systems"
-  name                       = "TrueNas Core"
+  tags = ["PRD"]
+  name                       = "nas.bsd.14.truenas"
   resource_pool_id           = data.vsphere_resource_pool.esxi_host_01_default_resource_pool.id
   datastore_id               = data.vsphere_datastore.esxi01_hdd_vm_datastore01.id
   num_cpus                   = 2
@@ -146,8 +122,8 @@ resource "vsphere_virtual_machine" "rhel_containers_misc" {
       enable_disk_uuid
     ]
   }
-  folder = "/Linux Servers"
-  name                       = "RHEL 9 - Miscellaneous Containers"
+  tags = ["PRD"]
+  name                       = "containers.rhel.9.general"
   resource_pool_id           = data.vsphere_resource_pool.esxi_host_02_default_resource_pool.id
   datastore_id               = data.vsphere_datastore.esxi02_ssd_vm_datastore01.id
   num_cpus                   = 2
@@ -177,8 +153,39 @@ resource "vsphere_virtual_machine" "rhel_onedev" {
       cdrom
     ]
   }
-  folder = "/Linux Servers"
-  name                       = "RHEL 9 - OneDev"
+  tags = ["PRD"]
+  name                       = "containers.rhel.9.onedev"
+  resource_pool_id           = data.vsphere_resource_pool.esxi_host_01_default_resource_pool.id
+  datastore_id               = data.vsphere_datastore.esxi01_ssd_vm_datastore01.id
+  num_cpus                   = 2
+  memory                     = 4096
+  guest_id                   = "rhel9_64Guest"
+  wait_for_guest_net_timeout = 0
+
+  network_interface {
+    network_id = data.vsphere_network.vm_network.id
+  }
+
+  disk {
+    label            = "ssd_disk0"
+    unit_number      = 0
+    size             = 150
+    thin_provisioned = true
+    datastore_id     = data.vsphere_datastore.esxi01_ssd_vm_datastore01.id
+  }
+}
+
+resource "vsphere_virtual_machine" "rhel_netbird_peer" {
+  lifecycle {
+    ignore_changes = [
+      ept_rvi_mode,
+      hv_mode,
+      enable_disk_uuid,
+      cdrom
+    ]
+  }
+  tags = ["PRD"]
+  name                       = "vpn.rhel.9.netbird_peer"
   resource_pool_id           = data.vsphere_resource_pool.esxi_host_01_default_resource_pool.id
   datastore_id               = data.vsphere_datastore.esxi01_ssd_vm_datastore01.id
   num_cpus                   = 2
@@ -208,8 +215,8 @@ resource "vsphere_virtual_machine" "rhel_authentik" {
       cdrom
     ]
   }
-  folder = "/Linux Servers"
-  name                       = "RHEL 9 - Authentik"
+  tags = ["PRD"]
+  name                       = "containers.rhel.9.authentik"
   resource_pool_id           = data.vsphere_resource_pool.esxi_host_01_default_resource_pool.id
   datastore_id               = data.vsphere_datastore.esxi01_hdd_vm_datastore01.id
   num_cpus                   = 2
@@ -239,8 +246,8 @@ resource "vsphere_virtual_machine" "fedora_citrix_client" {
       cdrom
     ]
   }
-  folder = "/Workstations"
-  name                       = "Fedora 39 Workstation - Citrix Client"
+  tags = ["PRD"]
+  name                       = "workstation.fedora.40.citrix_client"
   resource_pool_id           = data.vsphere_resource_pool.esxi_host_01_default_resource_pool.id
   datastore_id               = data.vsphere_datastore.esxi01_ssd_vm_datastore01.id
   num_cpus                   = 4
@@ -270,8 +277,8 @@ resource "vsphere_virtual_machine" "windows_server_2022_dc" {
       cdrom
     ]
   }
-  folder = "/Windows Servers"
-  name                       = "Windows Server 2022 DC"
+  tags = ["PRD"]
+  name                       = "dc.win.srv2022.domain_controller"
   resource_pool_id           = data.vsphere_resource_pool.esxi_host_02_default_resource_pool.id
   datastore_id               = data.vsphere_datastore.esxi02_ssd_vm_datastore01.id
   num_cpus                   = 4
